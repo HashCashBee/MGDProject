2a19bc347aca70507f2db763a668651d6408c1eb
NickBee <hamlemur@hotmail.co.uk>
2016-04-21 15:48:19 +0100

    Movement and basic collisions with walls implemented

--- GameMain.js
+++ GameMain.js
@@ -3,9 +3,30 @@ var context, tileSet,
     NUM_ROWS = 30,
     NUM_COLS = 40, sheep, wolf;
 
-function Player(x, y){
+var Key = {
+    _pressed:{},
+    LEFT: 37,
+UP: 38,
+RIGHT:39,
+DOWN:40,
+    isDown: function(keyCode){
+        return this._pressed[keyCode];
+    },
+    onKeydown: function(event){
+        this._pressed[event.keyCode] = true;
+    },
+    onKeyup: function(event){
+        delete this._pressed[event.keyCode];
+    }
+};
+
+window.addEventListener('keyup', function(event) { Key.onKeyup(event);}, false);
+window.addEventListener('keydown', function(event){ Key.onKeydown(event);}, false);
+
+function Player(x, y, playable){
     this.x = x;
     this.y = y;
+    this.playable = playable;
 }
 
 Player.prototype.load = function(img_file){
@@ -13,13 +34,24 @@ Player.prototype.load = function(img_file){
     this.image.src = img_file;
 };
 
+Player.prototype.update = function(){
+    if(this.playable) {
+        if (Key.isDown(Key.UP)) this.moveBy(0, -1);
+        if (Key.isDown(Key.LEFT)) this.moveBy(-1, 0);
+        if (Key.isDown(Key.RIGHT)) this.moveBy(1, 0);
+        if (Key.isDown(Key.DOWN)) this.moveBy(0, 1);
+    }
+};
+
 Player.prototype.draw = function(context){
-    context.drawImage(this.image, 0, 0, 32, 32, this.x * 32 + 16, this.y * 32 +16, 32, 32);
+    context.drawImage(this.image, 0, 0, 32, 32, this.x * 32, this.y * 32, 32, 32);
 };
 
 Player.prototype.moveBy = function(dx, dy){
-    this.x += dx;
-    this.y += dy;
+    if(tileSet.canMoveTo(this.x + dx, this.y + dy)) {
+        this.x += dx;
+        this.y += dy;
+    }
 };
 
 
@@ -28,6 +60,15 @@ function TileSet(rows, cols, size){
     this.rows = rows;
     this.cols = cols;
 }
+
+TileSet.prototype.canMoveTo = function(x, y){
+    if (tile_map[y][x] == 0)
+        return true;
+
+    else
+        return false;
+
+};
 
 TileSet.prototype.loadTiles = function(image_file, callback){
     this.image = new Image();
@@ -58,8 +99,8 @@ window.onload = function(){
 
 function LoadAssets(){
     tileSet = new TileSet(NUM_ROWS, NUM_COLS, TILE_SIZE);
-    sheep = new Player(13, 5);
-    wolf = new Player(20, 20);
+    sheep = new Player(13, 5, true);
+    wolf = new Player(20, 20, false);
     sheep.load("images/sheep-sprite.png");
     wolf.load("images/wolf-sprite.png");
     tileSet.loadTiles("images/sheep_tileset.png", gameLoop);
@@ -71,6 +112,8 @@ function drawPlayers(context){
 }
 
 function gameLoop(){
+    sheep.update();
+    wolf.update();
     tileSet.draw(context);
     drawPlayers(context);
     requestAnimationFrame(gameLoop);
